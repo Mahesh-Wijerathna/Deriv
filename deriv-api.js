e1922e15aff1b1374d2bc4dd2420e47be1fdd5b2
Mahesh-Wijerathna <maheshtharanga49@gmail.com>
2024-09-15 08:15:48 +0530

    jg

--- deriv-api.js
+++ deriv-api.js
@@ -110,12 +110,12 @@
 
 const express = require('express')
 const app = express()
-const port = 4000
+const PORT = 4000
 
 app.get('/', (req, res) => {
     res.send("hi")
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${PORT}`)
 })

379b9f16a2365c70ef262faa450ea6abfc1ec390
Mahesh-Wijerathna <maheshtharanga49@gmail.com>
2024-09-16 11:32:52 +0530

    hgfh

--- deriv-api.js
+++ deriv-api.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 const express = require('express');
-const e = require('express');
+const fs = require('fs');
+const winston = require('winston');
 const app = express();
 
 const PORT = 4000;
@@ -14,25 +15,24 @@ let data = [];
 let server = null;
 
 server = app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}
-    bearish and bullish engulfing pattern
-    15 simple moving average`);
+    logger.warn(`server started `);
+    console.log(`http://localhost:${PORT}    bearish and bullish engulfing pattern    15 simple moving average`);
 });
 app.get('/start', (req, res) => {
     if (ws) {
-        res.send("WebSocket connection already started");
+        res.send("WebSocket connection already started");        
         return; 
     }
     ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');    
     ws.on('open', () => {
         console.log('Trying to authorize');
+        logger.warn('Trying to authorize');
         ws.send(JSON.stringify({ authorize: apiToken }));
     });
     ws.on('message', (message) => {
         const response = JSON.parse(message);
         if (response.msg_type === 'ohlc') {
             date = new Date(response.ohlc.epoch* 1000);
-            // console.log(date.getMinutes(), date.getSeconds());
             if(date.getSeconds() === 0) {
                 if(bullishSignal)
                     bullishTrade();
@@ -40,16 +40,18 @@ app.get('/start', (req, res) => {
                     BearishTrade();
             }
             else if(date.getSeconds() === 58) {
-                // console.log('ready to buy');
                 data.push([
                     Number(response.ohlc.open),
                     Number(response.ohlc.close),
                     Number(response.ohlc.low),
                     Number(response.ohlc.high)
                 ]);
+                logger.info({candle : {open: response.ohlc.open, close: response.ohlc.close, low: response.ohlc.low, high: response.ohlc.high}});
                 bullishSignal = setBullishSignal();
                 bearishSignal = setBearishSignal();
+                logger.info({"bullishSignal": bullishSignal, "bearishSignal": bearishSignal}); 
             }
+            
             
         }
         else if (response.msg_type === 'candles') {
@@ -57,9 +59,11 @@ app.get('/start', (req, res) => {
             response.candles.forEach(candle => {                
                 data.push([candle.open, candle.close, candle.low, candle.high]);
             });
+            logger.info(response.candles);
         }      
         else if(response.msg_type === 'authorize') {
             console.log('Authorization was successful');
+            logger.warn('Authorization was successful');
             ws.send(JSON.stringify({
                 ticks_history: 'R_100', // Replace 'R_100' with the appropriate symbol
                 adjust_start_time: 1,
@@ -70,6 +74,7 @@ app.get('/start', (req, res) => {
                 granularity: 60 // 1-minute candles
             }));
             console.log('Requesting 20 candles');
+            logger.warn('Requesting 20 candles');
             ws.send(JSON.stringify({
                 ticks_history: 'R_100',
                 adjust_start_time: 1,
@@ -80,7 +85,8 @@ app.get('/start', (req, res) => {
                 granularity: 60, 
                 subscribe: 1
             })); 
-            console.log('Subscribing to 1-minute candles');           
+            console.log('Subscribing to 1-minute candles');  
+            logger.warn('Subscribing to 1-minute candles');         
         }
         else if(response.msg_type === 'buy') {
             console.log('Buy response:', response);
@@ -107,6 +113,24 @@ app.get('/close', (req, res) => {
         ws = null;
     }
     res.send("WebSocket connection closed");
+});
+app.get('/warn', (req, res) => {
+    fs.readFile('logs/warn.log', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading log file');
+        }
+        res.type('text/plain');  // Serve the logs as plain text
+        res.send(data);
+    });
+});
+app.get('/info', (req, res) => {
+    fs.readFile('logs/info.log', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading log file');
+        }
+        res.type('text/plain');  // Serve the logs as plain text
+        res.send(data);
+    });
 });
 app.get('/', (req, res) => {
     res.send({data: data});
@@ -162,16 +186,16 @@ const setBearishSignal = () => {
     
     const last = calculateAverage(data.length - 16, data.length - 2);
     const previous = calculateAverage(data.length - 15, data.length - 1);
-    
+    logger.info({"last": last, "previous": previous});
+    logger.info({"currentCandle": currentCandle, "previousCandle": previousCandle});
     if (isBearishEngulfing && last < previous) {
-        console.log("Bearish Engulfing Pattern Detected:", previousCandle, currentCandle);
+        logger.warn("Bearish Engulfing Pattern Detected:", previousCandle, currentCandle);
         return true;
     }
     return false;
 };
 const setBullishSignal = () => {
     if (data.length < 2) return 0;
-    // console.log('bullish');
     const [currentCandle, previousCandle] = [data[data.length - 1], data[data.length - 2]];
 
     const isBullishEngulfing = (
@@ -182,9 +206,10 @@ const setBullishSignal = () => {
     
     const last = calculateAverage(data.length - 16, data.length - 2);
     const previous = calculateAverage(data.length - 15, data.length - 1);
-    
+    logger.info({"last": last, "previous": previous});
+    logger.info({"currentCandle": currentCandle, "previousCandle": previousCandle});
     if (isBullishEngulfing && last > previous) {
-        console.log("Bullish Engulfing Pattern Detected:", previousCandle, currentCandle);
+        logger.warn("Bullish Engulfing Pattern Detected:", previousCandle, currentCandle);
         return true;
     }
     return false;
@@ -192,6 +217,7 @@ const setBullishSignal = () => {
 
 const calculateAverage = (startIndex, endIndex) => {
     // console.log(startIndex, endIndex, data.length);
+    // logger.info({"startIndex": startIndex, "endIndex": endIndex, "dataLength": data.length});
     if (startIndex < 0 || endIndex >= data.length || startIndex > endIndex) {
         return 0;
     }
@@ -202,5 +228,26 @@ const calculateAverage = (startIndex, endIndex) => {
         count++;
     }
     // console.log(startIndex, endIndex,count, sum/count);
+    // logger.info({"average": sum / count});
     return sum / count;
 };
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: () => new Date().toISOString() // ISO string in GMT 0
+        }),
+        winston.format.printf(({ level, message, timestamp }) => {
+            return JSON.stringify({
+                level: level,
+                timestamp: timestamp,
+                message: message
+            });
+        })
+    ),
+    transports: [
+        new winston.transports.File({ level: 'info', filename: 'logs/info.log' }),
+        new winston.transports.File({ level: 'warn', filename: 'logs/warn.log' }),
+    ],
+});

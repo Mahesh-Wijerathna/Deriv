b626dc02e41bca9df83e3605d8c69fbd211ceed5
Mahesh-Wijerathna <maheshtharanga49@gmail.com>
2024-09-20 11:20:40 +0530

    dhfhg

--- differ.js
+++ differ.js
@@ -17,150 +17,252 @@ let data = [];
 let lastWatchedTime = null;
 let socketStatus = 'closed';
 
-function getWatchTime(){
-    if(date)
-        lastWatchedTime = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} `
-    else
-        lastWatchedTime = 'Still NULL'
-}
-function takeContract (){
-    ws.send(JSON.stringify({
-        buy: 1,
-        price: 1,  // Maximum amount you're willing to stake
-        parameters: {
-            contract_type: 'DIGITDIFF',  // Last Digit Differ contract
-            symbol: 'R_10',              // Asset: Volatility Index 10
-            duration: 1,                 // Duration in ticks (5 ticks in this case)
-            duration_unit: 't',           // Duration in ticks
-            basis: 'stake',               // Stake-based trade
-            amount: 0.5,                    // Stake amount (1 USD in this case)
-            barrier: `${data[data.length-1]}`, // You are predicting that the last digit of the tick will differ from 5
-            currency: 'USD'               // Trading in USD
-        }
-    }));
-}
-function checking(){    
-    if(signal && used <= 3){
-        if(data[data.length-1] == data[data.length-2])
-        {
-            takeContract();   
-            signal = false;  
-            data = [-1,-2,-3,-4,-5];  
-            used++;
-            logger.warn('contract taken');    
-        }
+function getWatchTime() {
+    try {
+        if (date)
+            lastWatchedTime = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} `;
+        else
+            lastWatchedTime = 'Still NULL';
+    } catch (err) {
+        logger.error('Error in getWatchTime: ' + err.message);
     }
-    else if(data[data.length-1] == data[data.length-2]
-        && data[data.length-2] == data[data.length-3]){
-        signal = true;
-        data = [-1,-2,-3,-4,-5]; 
-        used = 0;
-        logger.warn('Signal detected');
-    }
-    if(data.length > 5){
-        data.shift();
-    }
-
 }
 
-// routes
-app.get('/error', (req, res) => { fs.readFile('logs/error.log', 'utf8', (err, data) => {res.type('text/plain');res.send(data); });});
-app.get('/warn', (req, res) => { fs.readFile('logs/warn.log', 'utf8', (err, data) => {res.type('text/plain');res.send(data); });});
-app.get('/info', (req, res) => { fs.readFile('logs/info.log', 'utf8', (err, data) => {res.type('text/plain');res.send(data); });});
-app.get('/restart',(req,res)=>{restartServer();res.send({"server Status" : 'restarted' }) });
-app.get('/close',(req,res)=>{closeSocket();res.send({"socket Status" : 'closed' }) });
-app.get('/open',(req,res)=>{openSocket();res.send({"socket Status" : socketStatus }) });
-app.get('/test',(req,res)=>{res.send({'max':max})});
-app.get('/',(req,res)=>{getWatchTime(); res.send({'last time data : ':{'time':lastWatchedTime,'data':data}})});
-// server 
-function restartServer(){
-    stopServer();
-    startServer();
-    logger.warn('server restarting ...');
+function takeContract() {
+    try {
+        ws.send(JSON.stringify({
+            buy: 1,
+            price: 1,
+            parameters: {
+                contract_type: 'DIGITDIFF',
+                symbol: 'R_10',
+                duration: 1,
+                duration_unit: 't',
+                basis: 'stake',
+                amount: 0.5,
+                barrier: `${data[data.length - 1]}`,
+                currency: 'USD'
+            }
+        }));
+    } catch (err) {
+        logger.error('Error in takeContract: ' + err.message);
+    }
 }
-function stopServer(){
-    server.close();
+
+function checking() {
+    try {
+        if (signal && used <= 3) {
+            if (data[data.length - 1] == data[data.length - 2]) {
+                takeContract();
+                signal = false;
+                data = [-1, -2, -3, -4, -5];
+                used++;
+                logger.warn('Contract taken');
+            }
+        } else if (data[data.length - 1] == data[data.length - 2] && data[data.length - 2] == data[data.length - 3]) {
+            signal = true;
+            data = [-1, -2, -3, -4, -5];
+            used = 0;
+            logger.warn('Signal detected');
+        }
+        if (data.length > 5) {
+            data.shift();
+        }
+    } catch (err) {
+        logger.error('Error in checking: ' + err.message);
+    }
 }
-function clearLogFile() {  
+
+// Routes
+app.get('/error', (req, res) => {
+    try {
+        fs.readFile('logs/error.log', 'utf8', (err, data) => {
+            res.type('text/plain');
+            res.send(data);
+        });
+    } catch (err) {
+        logger.error('Error in /error route: ' + err.message);
+    }
+});
+
+app.get('/warn', (req, res) => {
+    try {
+        fs.readFile('logs/warn.log', 'utf8', (err, data) => {
+            res.type('text/plain');
+            res.send(data);
+        });
+    } catch (err) {
+        logger.error('Error in /warn route: ' + err.message);
+    }
+});
+
+app.get('/info', (req, res) => {
+    try {
+        fs.readFile('logs/info.log', 'utf8', (err, data) => {
+            res.type('text/plain');
+            res.send(data);
+        });
+    } catch (err) {
+        logger.error('Error in /info route: ' + err.message);
+    }
+});
+
+app.get('/restart', (req, res) => {
+    try {
+        restartServer();
+        res.send({ "server Status": 'restarted' });
+    } catch (err) {
+        logger.error('Error in /restart route: ' + err.message);
+    }
+});
+
+app.get('/close', (req, res) => {
+    try {
+        closeSocket();
+        res.send({ "socket Status": 'closed' });
+    } catch (err) {
+        logger.error('Error in /close route: ' + err.message);
+    }
+});
+
+app.get('/open', (req, res) => {
+    try {
+        openSocket();
+        res.send({ "socket Status": socketStatus });
+    } catch (err) {
+        logger.error('Error in /open route: ' + err.message);
+    }
+});
+
+app.get('/test', (req, res) => {
+    try {
+        res.send({ 'max': max });
+    } catch (err) {
+        logger.error('Error in /test route: ' + err.message);
+    }
+});
+
+app.get('/', (req, res) => {
+    try {
+        getWatchTime();
+        res.send({ 'last time data': { 'time': lastWatchedTime, 'data': data } });
+    } catch (err) {
+        logger.error('Error in / route: ' + err.message);
+    }
+});
+
+// Server functions
+function restartServer() {
+    try {
+        stopServer();
+        startServer();
+        logger.warn('Server restarting...');
+    } catch (err) {
+        logger.error('Error in restartServer: ' + err.message);
+    }
+}
+
+function stopServer() {
+    try {
+        closeSocket();
+        server.close();
+    } catch (err) {
+        logger.error('Error in stopServer: ' + err.message);
+    }
+}
+
+function clearLogFile() {
     try {
         fs.writeFileSync('logs/info.log', '');
         fs.writeFileSync('logs/warn.log', '');
         fs.writeFileSync('logs/error.log', '');
-        logger.error('log files cleared');
-    }
-    catch (err) {
-        logger.error(err);
-    }
-}
-function closeSocket(){
-    if(ws != null){
-        ws.close();
-        ws = null;
-        socketStatus = 'closed';
-    }
-    else{
-        socketStatus = 'already closed';
+        logger.error('Log files cleared');
+    } catch (err) {
+        logger.error('Error in clearLogFile: ' + err.message);
     }
 }
-function watching(){
-    ws.on('message', (message) => {
-        const response = JSON.parse(message);
-        if (response.msg_type === 'tick') {
-            const tick = response.tick;
-            const value = parseInt(parseFloat(tick.quote)*1000);
-            data.push(value%10);
-            // logger.info({'value':parseFloat(tick.quote),'data':data});            
-            checking();
-            date = new Date(response.tick.epoch*1000);
+
+function closeSocket() {
+    try {
+        if (ws != null) {
+            ws.close();
+            ws = null;
+            socketStatus = 'closed';
+        } else {
+            socketStatus = 'already closed';
         }
-        else if(response.msg_type === 'authorize') {
-            logger.info('Authorization was successful');
-            ws.send(JSON.stringify({
-                ticks: 'R_10',  
-                subscribe: 1   
-            }));        
-            logger.warn('Subscribing ticks'); 
-        }
-        else if (response.msg_type === 'proposal_open_contract') {
-            const contract = response.proposal_open_contract;
-            if (contract.is_sold === 1) {
-                if (contract.profit > 0) {
-                    console.log(`You won! Profit: ${contract.profit}`);
-                } else {
-                    console.log(`You lost. Loss: ${contract.profit}`);
+    } catch (err) {
+        logger.error('Error in closeSocket: ' + err.message);
+    }
+}
+
+function watching() {
+    try {
+        ws.on('message', (message) => {
+            const response = JSON.parse(message);
+            if (response.msg_type === 'tick') {
+                const tick = response.tick;
+                const value = parseInt(parseFloat(tick.quote) * 1000);
+                data.push(value % 10);
+                logger.info({ 'value': parseFloat(tick.quote), 'data': data });
+                checking();
+                date = new Date(response.tick.epoch * 1000);
+            } else if (response.msg_type === 'authorize') {
+                logger.info('Authorization was successful');
+                ws.send(JSON.stringify({
+                    ticks: 'R_10',
+                    subscribe: 1
+                }));
+                logger.warn('Subscribing ticks');
+            } else if (response.msg_type === 'proposal_open_contract') {
+                const contract = response.proposal_open_contract;
+                if (contract.is_sold === 1) {
+                    if (contract.profit > 0) {
+                        console.log(`You won! Profit: ${contract.profit}`);
+                    } else {
+                        console.log(`You lost. Loss: ${contract.profit}`);
+                    }
                 }
+            } else if (response.msg_type === 'error') {
+                logger.error(response.error.message);
+            } else {
+                logger.warn({ 'unhandled response': response });
             }
-        }
-        else if(response.msg_type === 'error') {
-            logger.error(response.error.message);
-        }
-        else{
-            logger.warn({'unhandled response':response});
-        }
-    });
-    
-}
-function openSocket(){
-    if(ws != null){
-        socketStatus = 'already opened';
-        return;
-    }
-    else{
-        ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
-        watching(); 
-        ws.on('open', () => {
-            logger.info('Trying to authorize');
-            ws.send(JSON.stringify({ authorize: apiToken }));
-        }); 
-        socketStatus = 'just Opened'       
+        });
+    } catch (err) {
+        logger.error('Error in watching: ' + err.message);
     }
 }
-function startServer  () {  
-    clearLogFile();
-    openSocket();
-    server = app.listen(PORT, () => {        
-        logger.warn(`server started`);  
-    });
-};
+
+function openSocket() {
+    try {
+        if (ws != null) {
+            socketStatus = 'already opened';
+            return;
+        } else {
+            ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
+            watching();
+            ws.on('open', () => {
+                logger.info('Trying to authorize');
+                ws.send(JSON.stringify({ authorize: apiToken }));
+            });
+            socketStatus = 'just Opened';
+        }
+    } catch (err) {
+        logger.error('Error in openSocket: ' + err.message);
+    }
+}
+
+function startServer() {
+    try {
+        clearLogFile();
+        openSocket();
+        server = app.listen(PORT, () => {
+            logger.warn(`Server started on port ${PORT}`);
+        });
+    } catch (err) {
+        logger.error('Error in startServer: ' + err.message);
+    }
+}
 
 startServer();
